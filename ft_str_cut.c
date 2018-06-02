f332ab719ef38ea73c6fdaeb3694506e3c7cedb3
Dmytro SKRYPNYK <dskrypny@e2r8p2.unit.ua>
2018-06-02 18:23:34 +0300

    small norm

--- ft_str_cut.c
+++ ft_str_cut.c
@@ -6,7 +6,7 @@
 /*   By: dskrypny <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/22 18:22:52 by dskrypny          #+#    #+#             */
-/*   Updated: 2018/06/02 17:30:01 by dskrypny         ###   ########.fr       */
+/*   Updated: 2018/06/02 18:23:05 by dskrypny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 char	*ft_str_cut(char *str, size_t size)
 {
 	char	*res;
-	size_t		i;
+	size_t	i;
 
 	if (size == 0)
 		return (str);
